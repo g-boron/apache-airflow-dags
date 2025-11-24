49c248cd929eab3b5c0ae5691034aa7ebb22c425
Grzegorz Boron <51033813+g-boron@users.noreply.github.com>
2025-11-24 19:35:21 +0100

    feat(metrics): Refactored code
    CHANGED:
    - split metrics function into smaller
    - refactored code - added docstring and typings

--- dags/functions/operational_functions/dags/dags_parameters.py
+++ dags/functions/operational_functions/dags/dags_parameters.py
@@ -1,11 +1,17 @@
+""" Parsing DAGs configuration. """
 from airflow.models import Variable
 
 
 def get_dag_parameters(dag_name: str) -> dict:
-  """ """
+  """
+  Parses Airflow variable to get DAGs scheduler settings and description.
+
+  :param dag_name: DAG name set in Airflow variable.
+  :return: Dictionary of DAG settings.
+  """
   dags_parameters = Variable.get("dags_parameters", deserialize_json=True)
   dag_parameters = dags_parameters.get(dag_name)
   return {
     "scheduler": dag_parameters.get("scheduler"),
-    "description": dags_parameters.get("description")
+    "description": dag_parameters.get("description")
   }
--- dags/functions/operational_functions/files/files_cleaner.py
+++ dags/functions/operational_functions/files/files_cleaner.py
@@ -1,8 +1,18 @@
+""" Cleaning DAGs data directories. """
 import os
 import shutil
+import logging
+
+logger = logging.getLogger("airflow.task")
 
 
-def clean_data_directory(file_prefixes: tuple, **kwargs):
+def clean_data_directory(file_prefixes: tuple, **kwargs) -> None:
+  """
+  Cleans data directory.
+
+  :param file_prefixes: Files prefixes to clean.
+  :param kwargs: Config dictionary.
+  """
   files = [
     file for file in os.listdir(kwargs["data_path"])
     if file.startswith(file_prefixes)
@@ -12,4 +22,4 @@ def clean_data_directory(file_prefixes: tuple, **kwargs):
       os.remove(f"{kwargs['data_path']}{file}")
     except IsADirectoryError:
       shutil.rmtree(f"{kwargs['data_path']}{file}")
-  print("Files removed")
+  logger.info("Files removed")
