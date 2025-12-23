69bc69c753215f0faff9d6906a426de98fecb2d0
Grzegorz Boron <51033813+g-boron@users.noreply.github.com>
2025-12-23 20:41:58 +0100

    feat(unit-tests): Added Polars Benchmark transformations unit tests
    ADDED:
    - unit tests

---
+++ dags/tests/unit_tests/polars_benchmark/transform/test_calculate_speed.py
@@ -0,0 +1,68 @@
+""" UNIT TEST """
+from datetime import datetime, timedelta
+
+import polars as pl
+from pytest import approx
+
+from functions.polars_benchmark.functions.transform import calculate_speed
+
+
+columns = [
+  "id", "tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance"
+]
+
+
+def test_empty_frame() -> None:
+  sample_data = pl.DataFrame([], schema={
+    "id": pl.Int32,
+    "tpep_pickup_datetime": pl.Datetime,
+    "tpep_dropoff_datetime": pl.Datetime,
+    "trip_distance": pl.Float32
+  })
+  result_data = calculate_speed(sample_data)
+
+  assert result_data.is_empty()
+
+
+def test_calculate_speed() -> None:
+  sample_data = pl.DataFrame([
+    [
+      1, datetime(2025, 12, 3, 14, 0, 0),
+      datetime(2025, 12, 3, 14, 35, 0), 15
+    ],
+  ], schema=columns)
+  result_data = calculate_speed(sample_data)
+
+  assert result_data.shape == (1, 7)
+  row = result_data.filter(pl.col("id") == 1).row(0, named=True)
+  assert row["duration"] == timedelta(seconds=2100)
+  assert row["duration_hours"] == approx(0.583, rel=1e-3)
+  assert row["avg_speed"] == approx(25.71, rel=1e-3)
+
+
+def test_anomalies() -> None:
+  sample_data = pl.DataFrame([
+    [
+      1, datetime(2025, 12, 3, 14, 0, 0),
+      datetime(2025, 12, 3, 14, 35, 0), 15
+    ],
+    [
+      2, datetime(2025, 12, 1, 17, 0, 0),
+      datetime(2025, 12, 1, 17, 50, 0), 0
+    ],
+    [
+      3, datetime(2025, 12, 20, 20, 0, 0),
+      datetime(2025, 12, 20, 20, 0, 1), 15
+    ],
+  ], schema=columns)
+  result_data = calculate_speed(sample_data)
+
+  assert result_data.shape == (3, 7)
+  row = result_data.filter(pl.col("id") == 1).row(0, named=True)
+  assert row["duration"] == timedelta(seconds=2100)
+  assert row["duration_hours"] == approx(0.583, rel=1e-3)
+  assert row["avg_speed"] == approx(25.71, rel=1e-3)
+  assert result_data.filter(
+    (pl.col("id").is_in([2, 3])) &
+    (pl.col("avg_speed").is_null())
+  ).shape[0] == 2
---
+++ dags/tests/unit_tests/polars_benchmark/transform/test_create_daily_report.py
@@ -0,0 +1,67 @@
+""" UNIT TEST """
+from datetime import date
+
+import polars as pl
+from pytest import approx
+
+from functions.polars_benchmark.functions.transform import create_daily_report
+
+
+columns = [
+  "pickup_date", "pickup_day", "PULocationID", "DOLocationID",
+  "trip_distance", "fare_amount", "tip_amount", "tip_ratio", "passenger_count",
+  "VendorID", "avg_speed"
+]
+location_map_columns = ["LocationID", "Zone"]
+
+
+def test_empty_frame() -> None:
+  sample_data = pl.DataFrame([], schema=columns)
+  sample_locations = pl.DataFrame([], schema=location_map_columns)
+  sample_locations = sample_locations.with_columns(
+    pl.col("LocationID").cast(pl.Int64)
+  )
+  result_data = create_daily_report(sample_data, sample_locations)
+
+  assert result_data.is_empty()
+
+
+def test_create_daily_report() -> None:
+  sample_data = pl.DataFrame([
+    [
+      "2025-12-01", "Sunday", 2, 4, 15, 10, 2, 0.2, 4, "V1", 30
+    ],
+    [
+      "2025-12-01", "Sunday", 2, 4, 8, 8, 1, 0.125, 2, "V2", 20
+    ],
+    [
+      "2025-12-10", "Monday", 1, 3, 45, 20, 10, 0.5, 3, "V3", 60
+    ],
+    [
+      "2025-12-20", "Tuesday", 3, 99, 3, 2, 1, 0.5, 1, "V4", 15
+    ]
+  ], schema=columns)
+  sample_locations = pl.DataFrame([
+    [1, "Manhattan"],
+    [2, "SoHo"],
+    [3, "Greenwich Village"],
+    [4, "Brooklyn"]
+  ], schema=location_map_columns)
+  result_data = create_daily_report(sample_data, sample_locations)
+
+  assert result_data.shape == (3, 12)
+  row = result_data.filter(pl.col("pickup_date") == date(
+    2025, 12, 1)).row(0, named=True)
+  assert row["pickup_location"] == "SoHo"
+  assert row["dropoff_location"] == "Brooklyn"
+  assert row["mean_trip_distance"] == approx(11.5)
+  assert row["mean_fare_amount"] == 9
+  assert row["mean_tip_amount"] == approx(1.5)
+  assert row["sum_tip_amount"] == 3
+  assert row["mean_tip_ratio"] == approx(0.1625)
+  assert row["passenger_count"] == 6
+  assert row["trips_count"] == 2
+  assert row["mean_speed"] == 25
+  assert result_data.filter(pl.col("pickup_date") == date(2025, 12, 20)).select(
+    pl.col("dropoff_location")
+  ).row(0)[0] == "Unknown"
